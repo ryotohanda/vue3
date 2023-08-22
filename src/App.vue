d198e0ab87b738885778498560715d2bcc5618e2
ruot ogawa <ryotoogawa1999@gmail.com>
2023-08-23 05:55:09 +0900

    ああああ

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import { ref } from 'vue'
 const items = ref([
   {
     id: 1,
-    name: 'S3',
+    name: 'S3で自動デプロイ',
     description:
       '刻んだ野菜をアボカドと混ぜてディップに。こんがり焼いたバゲットとお召し上がりください。',
     price: 480,
